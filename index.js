d01ad4ffc528ac3fe2418e82831e2b24e28d1b07
netstermonir <monirhossain6163@gmail.com>
2024-06-04 18:28:27 +0600

    monogdb

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const connectedDb = require("./src/dbConfig/dbConfig");
 const errorMiddleware = require("./src/middleware/errorMiddleware");
 const router = require("./src/api/apiRoutes");
+const { default: mongoose } = require("mongoose");
 dotenv.config();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
@@ -20,13 +21,9 @@ app.use(errorMiddleware);
 
 //db url
 let dataBaseUrl = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.r1nyd.mongodb.net/stride?retryWrites=true&w=majority&appName=Cluster0`;
-connectedDb(dataBaseUrl)
-  .then(() => {
-    console.log("Database Connected");
-    app.listen(port, () => {
-      console.log(`app listening at http://localhost:${port}`);
-    });
-  })
-  .catch((e) => {
-    console.log(e);
+mongoose.connect(dataBaseUrl, {}).then(() => {
+  console.log("connected to db");
+  app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
   });
+});
